92c93777c7698da83ce9f1cd5de8fcc9b7b86e54
dev-manojbagari <dev.manojbagari@gmail.com>
2022-06-27 10:31:26 +0530

    Max value in binary tree [ negative values allowed ]

---
+++ src/main/java/algorithim/MaxBT.java
@@ -0,0 +1,31 @@
+package algorithim;
+
+import java.util.OptionalInt;
+
+public class MaxBT {
+	public TreeNode root = null;
+
+	OptionalInt maxBT(TreeNode root) {
+
+		if (root == null)
+			return OptionalInt.empty();
+
+		return OptionalInt.of(maxBTRecursive(root));
+	}
+
+	int maxBTRecursive(TreeNode root) {
+
+		if (root.left == null && root.right == null)
+			return root.value;
+
+		if (root.left == null || root.right == null) {
+			if (root.left != null)
+				return Math.max(root.value, maxBTRecursive(root.left));
+			else
+				return Math.max(root.value, maxBTRecursive(root.right));
+
+		}
+
+		return Math.max(root.value, Math.max(maxBTRecursive(root.left), maxBTRecursive(root.right)));
+	}
+}
---
+++ src/test/java/algorithim/MaxBTTest.java
@@ -0,0 +1,55 @@
+package algorithim;
+
+import static org.junit.Assert.*;
+
+import java.util.OptionalInt;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class MaxBTTest {
+
+	MaxBT bst;
+	
+	@Before
+	public void setUp() throws Exception {
+		bst = new MaxBT();
+	}
+
+	@Test
+	public void maxBTTest() {
+
+		assertEquals(OptionalInt.empty(), bst.maxBT(null));
+		
+		bst.root = new TreeNode(8);
+		bst.root.left =new TreeNode(4);
+		bst.root.left.left=new TreeNode(2);
+		bst.root.left.right=new TreeNode(6);
+		
+		assertEquals(8, bst.maxBT(bst.root).getAsInt());
+		
+		
+		bst.root.right =new TreeNode(12);
+		
+		bst.root.right.left =new TreeNode(10);
+		bst.root.right.right =new TreeNode(14);
+		assertEquals(14, bst.maxBT(bst.root).getAsInt());
+		
+		bst.root.right.right =null;
+		assertEquals(12, bst.maxBT(bst.root).getAsInt());
+
+
+		bst.root=null;
+		
+		bst.root = new TreeNode(-8);
+		bst.root.right =new TreeNode(-4);
+		bst.root.right.right=new TreeNode(-2);
+		bst.root.right.left=new TreeNode(-6);
+		
+		assertEquals(-2, bst.maxBT(bst.root).getAsInt());
+		
+	}
+
+}
