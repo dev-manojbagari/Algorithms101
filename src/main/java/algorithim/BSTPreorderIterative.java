042f8c273e9d52832273f65e5c32d57b9861ec4d
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-02 09:28:34 +0530

    bt preorder iterative

---
+++ src/main/java/algorithim/BSTPreorderIterative.java
@@ -0,0 +1,42 @@
+package algorithim;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BSTPreorderIterative {
+    public TreeNode root = null;
+
+    List<Integer> preOrder(TreeNode root) {
+
+	List<Integer> list = new ArrayList<>();
+
+	preOrder(root, list);
+
+	return list;
+    }
+
+    private void preOrder(TreeNode root, List<Integer> list) {
+
+	if (root == null)
+	    return;
+
+	Stack<TreeNode> stack = new Stack<>();
+	stack.add(root);
+
+	while (!stack.isEmpty()) {
+
+	    TreeNode curr = stack.pop();
+	    list.add(curr.value);
+
+	    if (curr.right != null)
+		stack.add(curr.right);
+
+	    if (curr.left != null)
+		stack.add(curr.left);
+
+	}
+
+    }
+
+}
---
+++ src/test/java/algorithim/BSTPreorderIterativeTest.java
@@ -0,0 +1,40 @@
+package algorithim;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BSTPreorderIterativeTest {
+
+    BSTPreorderIterative bst;
+
+    @Before
+    public void setUp() throws Exception {
+	bst = new BSTPreorderIterative();
+
+    }
+
+    @Test
+    public void bstPreorderRecursiveTest() {
+	bst.root = new TreeNode(1);
+	bst.root.left = new TreeNode(2);
+
+	bst.root.right = new TreeNode(3);
+	bst.root.right.left = new TreeNode(4);
+	bst.root.right.left.left = new TreeNode(5);
+	bst.root.right.right = new TreeNode(6);
+
+	bst.root.right.right.left = new TreeNode(7);
+	bst.root.right.right.right = new TreeNode(8);
+	List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+	List<Integer> list2 = bst.preOrder(bst.root);
+
+	assertEquals(list1, list2);
+
+    }
+
+}
