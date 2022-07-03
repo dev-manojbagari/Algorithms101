f50a8c4a3b01304882a848056e5de4185e2dd351
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-04 05:22:24 +0530

    seralize & deseralize bt

---
+++ src/main/java/algorithim/tree/BTSeralizeDeseralize.java
@@ -0,0 +1,58 @@
+package algorithim.tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BTSeralizeDeseralize {
+    public TreeNode root = null;
+
+    public String seralize(TreeNode root) {
+
+	if (root == null)
+	    return "$" + ",";
+
+	StringBuilder sb = new StringBuilder();
+	seralize(root, sb);
+
+	return sb.toString();
+    }
+
+    private void seralize(TreeNode root, StringBuilder sb) {
+
+	if (root == null) {
+	    sb.append("$").append(",");
+	    return;
+	}
+	sb.append(root.value).append(",");
+
+	seralize(root.left, sb);
+	seralize(root.right, sb);
+
+    }
+
+    public TreeNode deSeralize(String str) {
+
+	if (str.length() == 0)
+	    return null;
+
+	Queue<String> nodes = new LinkedList<>(Arrays.asList(str.split(",")));
+
+	return deSeralize(nodes);
+    }
+
+    private TreeNode deSeralize(Queue<String> nodes) {
+	String str = nodes.poll();
+
+	if (nodes.isEmpty() || str == null || "$".equals(str))
+	    return null;
+
+	TreeNode node = new TreeNode(Integer.valueOf(str));
+
+	node.left = deSeralize(nodes);
+	node.right = deSeralize(nodes);
+
+	return node;
+    }
+
+}
---
+++ src/test/java/algorithim/tree/BTSeralizeDeseralizeTest.java
@@ -0,0 +1,65 @@
+package algorithim.tree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BTSeralizeDeseralizeTest {
+
+    BTSeralizeDeseralize bst;
+
+    @Before
+    public void setUp() throws Exception {
+	bst = new BTSeralizeDeseralize();
+
+    }
+
+    @Test
+    public void btSeralizeDeseralizeTest() {
+
+	bst.root = new TreeNode(3);
+
+	bst.root.left = new TreeNode(2);
+
+	bst.root.right = new TreeNode(5);
+
+	bst.root = bst.deSeralize(bst.seralize(bst.root));
+
+	assertEquals(bst.root.value, 3);
+	assertEquals(bst.root.left.value, 2);
+	assertEquals(bst.root.right.value, 5);
+
+    }
+
+    @Test
+    public void btSeralizeTest() {
+
+	bst.root = new TreeNode(3);
+
+	bst.root.left = new TreeNode(2);
+
+	bst.root.right = new TreeNode(5);
+
+	String str = bst.seralize(bst.root);
+
+	String expected = "3,2,$,$,5,$,$,";
+
+	assertEquals(str, expected);
+
+    }
+
+    @Test
+    public void btDeseralizeTest() {
+
+	String str = "3,2,$,$,5,$,$,";
+
+	bst.root = bst.deSeralize(str);
+
+	assertEquals(bst.root.value, 3);
+	assertEquals(bst.root.left.value, 2);
+	assertEquals(bst.root.right.value, 5);
+
+    }
+
+}
