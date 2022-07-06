82dcf321387c5d6945277a349cc930372506d12b
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-06 08:49:45 +0530

    Refactor: BT vaildate postorder [Recursive + Iterative]

--- src/test/java/algorithim/tree/BTVaildatePostorderTest.java
+++ src/test/java/algorithim/tree/BTVaildatePostorderTest.java
@@ -9,6 +9,10 @@ import org.junit.Test;
 public class BTVaildatePostorderTest {
 
     BTVaildatePostorder bst;
+    int[] post1 = { 2, 3, 4 };
+    int[] post2 = { 1, 4, 2 };
+    int[] post3 = { 1, 3, 4, 2, 7, 6, 5 };
+    int[] post4 = { 1, 3, 4, 6, 7, 2, 5 };
 
     @Before
     public void setUp() throws Exception {
@@ -19,54 +23,39 @@ public class BTVaildatePostorderTest {
     @Test
     public void btVaildatePostorderRecursiveTest() {
 
-	int[] pre1 = { 2, 3, 4 };
+	assertTrue(bst.vaildatePostOrderRecursive(post1));
 
-	assertTrue(bst.vaildatePostOrderRecursive(pre1));
+	assertTrue(bst.vaildatePostOrderRecursive(post2));
 
-	int[] pre2 = { 1, 4, 2 };
-	assertTrue(bst.vaildatePostOrderRecursive(pre2));
+	assertTrue(bst.vaildatePostOrderRecursive(post3));
 
-	int[] pre3 = { 1, 3, 4, 2, 7, 6, 5 };
-	assertTrue(bst.vaildatePostOrderRecursive(pre3));
-
-	int[] pre4 = { 1, 3, 4, 6, 7, 2, 5 };
-	assertFalse(bst.vaildatePostOrderRecursive(pre4));
+	assertFalse(bst.vaildatePostOrderRecursive(post4));
 
     }
 
     @Test
     public void btVaildatePostorderRecursiveMinMaxTest() {
 
-	int[] pre1 = { 2, 3, 4 };
+	assertTrue(bst.vaildatePostOrderRecursiveMinMax(post1));
 
-	assertTrue(bst.vaildatePostOrderRecursiveMinMax(pre1));
+	assertTrue(bst.vaildatePostOrderRecursiveMinMax(post2));
 
-	int[] pre2 = { 1, 4, 2 };
-	assertTrue(bst.vaildatePostOrderRecursiveMinMax(pre2));
+	assertTrue(bst.vaildatePostOrderRecursiveMinMax(post3));
 
-	int[] pre3 = { 1, 3, 4, 2, 7, 6, 5 };
-	assertTrue(bst.vaildatePostOrderRecursiveMinMax(pre3));
-
-	int[] pre4 = { 1, 3, 4, 6, 7, 2, 5 };
-	assertFalse(bst.vaildatePostOrderRecursiveMinMax(pre4));
+	assertFalse(bst.vaildatePostOrderRecursiveMinMax(post4));
 
     }
 
     @Test
     public void btVaildatePostorderIterativeTest() {
 
-	int[] pre1 = { 2, 3, 4 };
+	assertTrue(bst.vaildatePostOrderIterative(post1));
 
-	assertTrue(bst.vaildatePostOrderIterative(pre1));
+	assertTrue(bst.vaildatePostOrderIterative(post2));
 
-	int[] pre2 = { 1, 4, 2 };
-	assertTrue(bst.vaildatePostOrderIterative(pre2));
+	assertTrue(bst.vaildatePostOrderIterative(post3));
 
-	int[] pre3 = { 1, 3, 4, 2, 7, 6, 5 };
-	assertTrue(bst.vaildatePostOrderIterative(pre3));
-
-	int[] pre4 = { 1, 3, 4, 6, 7, 2, 5 };
-	assertFalse(bst.vaildatePostOrderIterative(pre4));
+	assertFalse(bst.vaildatePostOrderIterative(post4));
 
     }
 }
