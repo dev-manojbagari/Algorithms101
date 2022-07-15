a8f63c6316292e07f14e3000fa934d16d3858b7d
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-15 06:35:31 +0530

    BT Top & Bottom View

---
+++ src/main/java/algorithim/tree/BTTopViewBottomView.java
@@ -0,0 +1,122 @@
+package algorithim.tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BTTopViewBottomView {
+    public TreeNode root = null;
+
+    private class Pair {
+
+	TreeNode node;
+	int hd;
+
+	public Pair(TreeNode node, int hd) {
+	    this.node = node;
+	    this.hd = hd;
+	}
+    }
+
+    public List<Integer> btTopView(TreeNode root) {
+
+	List<Integer> list = new ArrayList<>();
+
+	if (root == null)
+	    return list;
+
+	Queue<Pair> q = new LinkedList<>();
+
+	HashMap<Integer, Integer> map = new HashMap<>();
+
+	q.add(new Pair(root, 0));
+	int minHd = Integer.MAX_VALUE;
+	int maxHd = Integer.MIN_VALUE;
+	while (!q.isEmpty()) {
+
+	    int levelSize = q.size();
+
+	    while (levelSize != 0) {
+
+		Pair temp = q.poll();
+		TreeNode node = temp.node;
+		int hd = temp.hd;
+		levelSize--;
+
+		if (!map.containsKey(hd)) {
+		    map.put(hd, node.value);
+
+		    if (minHd > hd)
+			minHd = hd;
+		    if (hd > maxHd)
+			maxHd = hd;
+
+		}
+
+		if (node.left != null)
+		    q.add(new Pair(node.left, hd - 1));
+
+		if (node.right != null)
+		    q.add(new Pair(node.right, hd + 1));
+
+	    }
+
+	}
+	for (int i = minHd; i <= maxHd; i++) {
+	    list.add(map.get(i));
+	}
+
+	return list;
+
+    }
+
+    public List<Integer> btBottomView(TreeNode root) {
+	List<Integer> list = new ArrayList<>();
+
+	if (root == null)
+	    return list;
+
+	Queue<Pair> q = new LinkedList<>();
+
+	HashMap<Integer, Integer> map = new HashMap<>();
+
+	q.add(new Pair(root, 0));
+	int minHd = Integer.MAX_VALUE;
+	int maxHd = Integer.MIN_VALUE;
+	while (!q.isEmpty()) {
+
+	    int levelSize = q.size();
+
+	    while (levelSize != 0) {
+
+		Pair temp = q.poll();
+		TreeNode node = temp.node;
+		int hd = temp.hd;
+		levelSize--;
+
+		map.put(hd, node.value);
+
+		if (minHd > hd)
+		    minHd = hd;
+		if (hd > maxHd)
+		    maxHd = hd;
+
+		if (node.left != null)
+		    q.add(new Pair(node.left, hd - 1));
+
+		if (node.right != null)
+		    q.add(new Pair(node.right, hd + 1));
+
+	    }
+
+	}
+	for (int i = minHd; i <= maxHd; i++) {
+	    list.add(map.get(i));
+	}
+
+	return list;
+    }
+
+}
