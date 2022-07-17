6358ec1dc98e645ac0f6a433025bb6b28e5fdabb
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-18 04:27:11 +0530

    BT is sum tree

---
+++ src/main/java/algorithim/tree/BTIsSumTree.java
@@ -0,0 +1,43 @@
+package algorithim.tree;
+
+public class BTIsSumTree {
+    public TreeNode root = null;
+
+    private class Sum {
+	int s;
+    }
+
+    // O(n)
+    public boolean isSumTree(TreeNode root) {
+	if (root == null)
+	    return true;
+
+	return isSumTree(root, new Sum());
+
+    }
+
+    public boolean isSumTree(TreeNode root, Sum sum) {
+	if (root == null)
+	    return true;
+
+	Sum ls = new Sum();
+	Sum rs = new Sum();
+
+	boolean l = isSumTree(root.left, ls);
+	if (!l)
+	    return false;
+
+	boolean r = isSumTree(root.right, rs);
+	if (!r)
+	    return false;
+
+	sum.s = root.value + ls.s + rs.s;
+
+	if (root.left != null && root.right != null)
+	    if (root.value != ls.s + rs.s)
+		return false;
+
+	return l && r;
+    }
+
+}
---
+++ src/test/java/algorithim/tree/BTIsSumTreeTest.java
@@ -0,0 +1,46 @@
+package algorithim.tree;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BTIsSumTreeTest {
+
+    BTIsSumTree bt;
+
+    @Before
+    public void setUp() throws Exception {
+	bt = new BTIsSumTree();
+    }
+
+    @Test
+    public void test() {
+
+	bt.root = new TreeNode(1);
+	bt.root.left = new TreeNode(2);
+	bt.root.left.left = new TreeNode(4);
+	bt.root.left.right = new TreeNode(5);
+	bt.root.right = new TreeNode(3);
+	bt.root.right.left = new TreeNode(6);
+	bt.root.right.right = new TreeNode(7);
+
+	assertFalse(bt.isSumTree(bt.root));
+    }
+
+    @Test
+    public void tes2() {
+
+	bt.root = new TreeNode(44);
+	bt.root.left = new TreeNode(9);
+	bt.root.left.left = new TreeNode(4);
+	bt.root.left.right = new TreeNode(5);
+	bt.root.right = new TreeNode(13);
+	bt.root.right.left = new TreeNode(6);
+	bt.root.right.right = new TreeNode(7);
+
+	assertTrue(bt.isSumTree(bt.root));
+    }
+
+}
