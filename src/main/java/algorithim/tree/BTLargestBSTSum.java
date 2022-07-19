2471601534366195e95c77912bd42efc407e0a22
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-19 14:02:24 +0530

    BT largest bst sum

---
+++ src/main/java/algorithim/tree/BTLargestBSTSum.java
@@ -0,0 +1,49 @@
+package algorithim.tree;
+
+public class BTLargestBSTSum {
+    public TreeNode root = null;
+
+    private class Tree {
+	boolean isBST = true;
+	int sum = 0;
+	int min = Integer.MAX_VALUE;
+	int max = Integer.MIN_VALUE;
+	int maxSum = 0;
+    }
+
+    public int maxSumBST(TreeNode root) {
+
+	if (root == null)
+	    return 0;
+	Tree tree = new Tree();
+	maxSumBST(root, tree);
+
+	return tree.maxSum;
+    }
+
+    private void maxSumBST(TreeNode root, Tree tree) {
+
+	if (root == null)
+	    return;
+
+	Tree lst = new Tree();
+	Tree rst = new Tree();
+
+	maxSumBST(root.left, lst);
+	maxSumBST(root.right, rst);
+	if (lst.isBST == true && rst.isBST == true && lst.max < root.value && root.value < rst.min) {
+	    tree.isBST = true;
+
+	    tree.sum = lst.sum + root.value + rst.sum;
+	    tree.maxSum = Math.max(Math.max(lst.maxSum, rst.maxSum), tree.sum);
+
+	    tree.min = root.left != null ? lst.min : root.value;
+	    tree.max = root.right != null ? rst.max : root.value;
+	    return;
+	}
+
+	tree.isBST = false;
+	tree.maxSum = Math.max(lst.maxSum, rst.maxSum);
+    }
+
+}
---
+++ src/test/java/algorithim/tree/BTLargestBSTSumTest.java
@@ -0,0 +1,74 @@
+package algorithim.tree;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BTLargestBSTSumTest {
+
+    BTLargestBSTSum bt;
+
+    @Before
+    public void setUp() throws Exception {
+	bt = new BTLargestBSTSum();
+
+    }
+
+    @Test
+    public void test() {
+	bt.root = new TreeNode(4);
+	bt.root.left = new TreeNode(3);
+	bt.root.left.left = new TreeNode(1);
+	bt.root.left.right = new TreeNode(2);
+
+	assertEquals(2, bt.maxSumBST(bt.root));
+    }
+
+    @Test
+    public void test2() {
+	bt.root = new TreeNode(5);
+	bt.root.left = new TreeNode(4);
+	bt.root.right = new TreeNode(8);
+
+	bt.root.left.left = new TreeNode(3);
+	bt.root.right.left = new TreeNode(6);
+	bt.root.right.right = new TreeNode(3);
+
+	assertEquals(7, bt.maxSumBST(bt.root));
+    }
+
+    @Test
+    public void test3() {
+	bt.root = new TreeNode(4);
+	bt.root.left = new TreeNode(8);
+	bt.root.left.left = new TreeNode(6);
+	bt.root.left.left.left = new TreeNode(9);
+	;
+	bt.root.left.right = new TreeNode(1);
+	bt.root.left.right.left = new TreeNode(-5);
+	bt.root.left.right.right = new TreeNode(4);
+	bt.root.left.right.left.right = new TreeNode(-3);
+	bt.root.left.right.right.right = new TreeNode(10);
+
+	assertEquals(14, bt.maxSumBST(bt.root));
+    }
+
+    @Test
+    public void test4() {
+	String str = "9,2,3,null,2,null,null,3,null,-5,4,null,1,null,10";
+	
+	bt.root = BTLeetCodeArrayFormatToBT.convert(str);
+	assertEquals(14, bt.maxSumBST(bt.root));
+
+    }
+    
+    @Test
+    public void test5() {
+	String str = "0,2,9,null,null,8,1,-1,null,null,4,-5,2,null,1,null,-3,1,3,3,0";
+	
+	bt.root = BTLeetCodeArrayFormatToBT.convert(str);
+	assertEquals(6, bt.maxSumBST(bt.root));
+
+    }
+}
