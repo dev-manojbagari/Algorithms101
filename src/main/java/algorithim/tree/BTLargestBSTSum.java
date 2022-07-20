0538c9d0933b5b6ff3966988ac16cce96ece9646
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-20 08:24:38 +0530

    refactore max bst sum in bt

--- src/main/java/algorithim/tree/BTLargestBSTSum.java
+++ src/main/java/algorithim/tree/BTLargestBSTSum.java
@@ -5,23 +5,23 @@ public class BTLargestBSTSum {
 
     private class Tree {
 	boolean isBST = true;
-	int sum = 0;
 	int min = Integer.MAX_VALUE;
 	int max = Integer.MIN_VALUE;
-	int maxSum = 0;
+	int sum;
     }
 
     public int maxSumBST(TreeNode root) {
-
 	if (root == null)
 	    return 0;
-	Tree tree = new Tree();
-	maxSumBST(root, tree);
 
-	return tree.maxSum;
+	int[] maxSum = { 0 };
+
+	maxSumBST(root, new Tree(), maxSum);
+
+	return maxSum[0];
     }
 
-    private void maxSumBST(TreeNode root, Tree tree) {
+    private void maxSumBST(TreeNode root, Tree tree, int[] maxSum) {
 
 	if (root == null)
 	    return;
@@ -29,21 +29,22 @@ public class BTLargestBSTSum {
 	Tree lst = new Tree();
 	Tree rst = new Tree();
 
-	maxSumBST(root.left, lst);
-	maxSumBST(root.right, rst);
+	maxSumBST(root.left, lst, maxSum);
+	maxSumBST(root.right, rst, maxSum);
+
 	if (lst.isBST == true && rst.isBST == true && lst.max < root.val && root.val < rst.min) {
+
 	    tree.isBST = true;
 
+	    tree.min = root.left == null ? root.val : lst.min;
+	    tree.max = root.right == null ? root.val : rst.max;
 	    tree.sum = lst.sum + root.val + rst.sum;
-	    tree.maxSum = Math.max(Math.max(lst.maxSum, rst.maxSum), tree.sum);
+	    maxSum[0] = Math.max(maxSum[0], tree.sum);
 
-	    tree.min = root.left != null ? lst.min : root.val;
-	    tree.max = root.right != null ? rst.max : root.val;
-	    return;
+	} else {
+	    tree.isBST = false;
 	}
 
-	tree.isBST = false;
-	tree.maxSum = Math.max(lst.maxSum, rst.maxSum);
     }
 
 }
