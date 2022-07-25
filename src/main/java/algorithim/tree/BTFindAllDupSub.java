4d12755463210b0804551060d80e671f77d5ab40
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-25 06:30:11 +0530

    Find all Duplicate subtrees in a Binary tree

---
+++ src/main/java/algorithim/tree/BTFindAllDupSub.java
@@ -0,0 +1,40 @@
+package algorithim.tree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class BTFindAllDupSub {
+    public TreeNode root = null;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+
+	if (root == null)
+	    return null;
+	List<TreeNode> list = new ArrayList<>();
+	findDuplicateSubtrees(root, new HashMap<String, Integer>(), list);
+
+	return list;
+    }
+
+    private String findDuplicateSubtrees(TreeNode root, HashMap<String, Integer> map, List<TreeNode> list) {
+
+	if (root == null) {
+	    return "#";
+	}
+
+	String leftST = findDuplicateSubtrees(root.left, map, list);
+	String rightST = findDuplicateSubtrees(root.right, map, list);
+
+	StringBuilder sb = new StringBuilder(leftST).append(",").append(rightST).append(",").append(root.val);
+	String curSubTree = sb.toString();
+	if (curSubTree.length() >= 5 && map.containsKey(curSubTree) && map.get(curSubTree) == 1)
+	    list.add(root);
+
+	map.put(curSubTree, map.getOrDefault(curSubTree, 0) + 1);
+
+	return curSubTree;
+
+    }
+
+}
---
+++ src/test/java/algorithim/tree/BTFindAllDupSubTest.java
@@ -0,0 +1,50 @@
+package algorithim.tree;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BTFindAllDupSubTest {
+
+    BTFindAllDupSub bt;
+
+    @Before
+    public void setUp() throws Exception {
+	bt = new BTFindAllDupSub();
+    }
+
+//    @Test
+//    public void test() {
+//
+//	bt.root = BTUtil.convert("1 2 3 4 -1 2 4 -1 -1 4 -1 -1 -1 -1 -1");
+//	List<Integer> list = Arrays.asList(2, 4);
+//	assertTrue(list.containsAll(bt.findDuplicateSubtrees(bt.root)));
+//
+//    }
+//
+//    @Test
+//    public void test2() {
+//	bt.root = BTUtil.convert("1 5 9 3 3 -1 -1 -1 -1 -1 -1");
+//	List<Integer> list = Arrays.asList(3);
+//	assertTrue(list.containsAll(bt.findDuplicateSubtrees(bt.root)));
+//    }
+
+    @Test
+    public void test3() {
+	bt.root = BTUtil.convert("0,0,0,0,null,null,0,null,null,null,0");
+	List<Integer> list = Arrays.asList(0);
+	assertEquals(list, bt.findDuplicateSubtrees(bt.root));
+    }
+
+//    @Test
+//    public void test4() {
+//	bt.root = BTUtil.convert("10,2,22,1,12,1,1");
+//	List<Integer> list = Arrays.asList(1);
+//	assertEquals(list, bt.findDuplicateSubtrees(bt.root));
+//    }
+
+}
