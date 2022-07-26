ec57ebaedb5406b81016309c865cceaf756919b7
dev-manojbagari <dev.manojbagari@gmail.com>
2022-07-26 08:36:38 +0530

    LL split alternately

---
+++ src/main/java/algorithim/linkedlist/LinkedListSplitAlternately.java
@@ -0,0 +1,49 @@
+package algorithim.linkedlist;
+
+import algorithim.linkedlist.LinkedList.Node;
+
+public class LinkedListSplitAlternately {
+
+    LinkedList.Node head, head1, head2;
+
+    public void splitAlternately() {
+
+	if (head == null)
+	    return;
+
+	Node curr = head;
+	Node ptr1 = head1;
+	Node ptr2 = head2;
+	int pos = 1;
+
+	while (curr != null) {
+
+	    Node temp = curr;
+	    curr = curr.next;
+	    temp.next = null;
+
+	    if (pos % 2 != 0) {
+
+		if (head1 == null) {
+		    head1 = ptr1 = temp;
+		} else {
+		    ptr1.next = temp;
+		    ptr1 = ptr1.next;
+		}
+
+	    } else {
+		if (head2 == null) {
+		    head2 = ptr2 = temp;
+		} else {
+		    ptr2.next = temp;
+		    ptr2 = ptr2.next;
+		}
+	    }
+	    pos++;
+
+	}
+	head = null;
+
+    }
+
+}
---
+++ src/test/java/algorithim/linkedlist/LinkedListSplitAlternatelyTest.java
@@ -0,0 +1,48 @@
+package algorithim.linkedlist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListSplitAlternatelyTest {
+    LinkedListSplitAlternately list;
+
+    @Before
+    public void setUp() throws Exception {
+	list = new LinkedListSplitAlternately();
+    }
+
+    @Test
+    public void test() {
+	list.head = LLUtil.convert("1 2 3 4 5 6");
+
+	list.splitAlternately();
+
+	assertEquals(1, list.head1.val);
+	assertEquals(3, list.head1.next.val);
+	assertEquals(5, list.head1.next.next.val);
+	assertNull(list.head1.next.next.next);
+
+	assertEquals(2, list.head2.val);
+	assertEquals(4, list.head2.next.val);
+	assertEquals(6, list.head2.next.next.val);
+	assertNull(list.head2.next.next.next);
+
+    }
+
+    @Test
+    public void test2() {
+	list.head = LLUtil.convert("1");
+
+	list.splitAlternately();
+
+	assertEquals(1, list.head1.val);
+
+	assertNull(list.head1.next);
+
+	assertNull(list.head2);
+
+    }
+}
