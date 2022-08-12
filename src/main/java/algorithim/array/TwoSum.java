45242c01f79155f3aaa3769b2166d837f8935ead
dev-manojbagari <dev.manojbagari@gmail.com>
2022-08-12 13:04:43 +0530

    Array check if two eleements add up to target value return their indices

---
+++ src/main/java/algorithim/array/TwoSum.java
@@ -0,0 +1,33 @@
+package algorithim.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+public class TwoSum {
+
+    public int[] toSum(int[] arr, int target) {
+
+	Map<Integer, Integer> map = new HashMap<>();
+
+	for (int i = 0; i < arr.length; i++) {
+
+	    if (map.containsKey(target - arr[i])) {
+		return new int[] { map.get(target - arr[i]), i };
+	    } else
+		map.put(arr[i], i);
+
+	}
+
+	return null;
+    }
+
+}
---
+++ src/test/java/algorithim/array/TwoSumTest.java
@@ -0,0 +1,27 @@
+package algorithim.array;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwoSumTest {
+
+    TwoSum array;
+
+    @Before
+    public void setUp() throws Exception {
+	array = new TwoSum();
+    }
+
+    @Test
+    public void test() {
+	int[] arr = { 2, 7, 11, 15 };
+
+	int[] indices = array.toSum(arr, 9);
+
+	Assert.assertEquals(Arrays.toString(new int[] { 0, 1 }), Arrays.toString(indices));
+    }
+
+}
