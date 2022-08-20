2fdc66e4d37aa07382164e3ba0d3db6278347b07
dev-manojbagari <dev.manojbagari@gmail.com>
2022-08-20 12:02:41 +0530

    --amend

--- src/main/java/algorithim/array/NextGreaterElement.java
+++ src/main/java/algorithim/array/NextGreaterElement.java
@@ -1,5 +1,6 @@
 package algorithim.array;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class NextGreaterElement {
@@ -9,15 +10,13 @@ public class NextGreaterElement {
 	Stack<Integer> stack = new Stack<>();
 
 	int[] nextGreater = new int[arr.length];
+	Arrays.fill(nextGreater, -1);
 
 	for (int i = 0; i < nextGreater.length; i++) {
 	    while (!stack.isEmpty() && arr[stack.peek()] < arr[i])
 		nextGreater[stack.pop()] = i;
 	    stack.push(i);
 	}
-
-	while (!stack.isEmpty())
-	    nextGreater[stack.pop()] = -1;
 
 	return nextGreater;
     }
