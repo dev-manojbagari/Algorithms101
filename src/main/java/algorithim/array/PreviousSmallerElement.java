36e94d1baebdb0bf6f4162cfbfe90f8b9def8662
dev-manojbagari <dev.manojbagari@gmail.com>
2022-08-20 12:37:04 +0530

    Array: previous smaller

---
+++ src/main/java/algorithim/array/PreviousSmallerElement.java
@@ -0,0 +1,27 @@
+package algorithim.array;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class PreviousSmallerElement {
+
+    public int[] previousSmallerElement(int[] arr) {
+	Stack<Integer> stack = new Stack<>();
+
+	int[] previousSmaller = new int[arr.length];
+	Arrays.fill(previousSmaller, -1);
+
+	for (int i = 0; i < previousSmaller.length; i++) {
+	    while (!stack.isEmpty() && arr[stack.peek()] >= arr[i])
+		stack.pop();
+
+	    if (!stack.isEmpty())
+		previousSmaller[i] = stack.peek();
+
+	    stack.push(i);
+	}
+
+	return previousSmaller;
+    }
+
+}
---
+++ src/test/java/algorithim/array/PreviousSmallerElementTest.java
@@ -0,0 +1,25 @@
+package algorithim.array;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PreviousSmallerElementTest {
+
+    PreviousSmallerElement array;
+
+    @Before
+    public void setUp() throws Exception {
+	array = new PreviousSmallerElement();
+    }
+
+    @Test
+    public void test() {
+	int[] NSE = array.previousSmallerElement(new int[] { 13, 8, 1, 5, 2, 5, 9, 7, 6, 12 });
+	Assert.assertEquals(Arrays.toString(new int[] { -1, -1, -1, 2, 2, 4, 5, 5, 5, 8 }), Arrays.toString(NSE));
+
+    }
+
+}
