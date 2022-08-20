10847801be9caa601f467bae7c5eed65c649f5d8
dev-manojbagari <dev.manojbagari@gmail.com>
2022-08-20 13:18:12 +0530

    --amend

--- src/main/java/algorithim/array/PreviousSmallerElement.java
+++ src/main/java/algorithim/array/PreviousSmallerElement.java
@@ -24,4 +24,25 @@ public class PreviousSmallerElement {
 	return previousSmaller;
     }
 
+    public int[] previousSmallerElement2(int[] arr) {
+	Stack<Integer> stack = new Stack<>();
+
+	int[] previousSmaller = new int[arr.length];
+	// Arrays.fill(previousSmaller, -1);
+
+	for (int i = 0; i < previousSmaller.length; i++) {
+	    while (!stack.isEmpty() && arr[stack.peek()] >= arr[i])
+		stack.pop();
+
+	    if (!stack.isEmpty())
+		previousSmaller[i] = stack.peek();
+	    else
+		previousSmaller[i] = -1;
+
+	    stack.push(i);
+	}
+
+	return previousSmaller;
+    }
+
 }
