3dad93de5685d1747e9e7eb92387e0b8d603ac9f
dev-manojbagari <dev.manojbagari@gmail.com>
2022-11-18 23:12:08 +0530

    refactor ll rotate left

--- src/main/java/algorithim/linkedlist/LinkedListRotateRight.java
+++ src/main/java/algorithim/linkedlist/LinkedListRotateRight.java
@@ -38,4 +38,65 @@ public class LinkedListRotateRight {
 	return head;
     }
 
+    public Node rotateRight(Node head, int k) {
+	if (head == null || k == 0)
+	    return head;
+
+	int len = getLen(head);
+	k = k % len;
+	if (k == 0)
+	    return head;
+
+	Node ptr1 = head;
+	Node ptr2 = head;
+	int count = 1;
+	while (count != k + 1) {
+	    count++;
+	    ptr2 = ptr2.next;
+	}
+
+	while (ptr2.next != null) {
+	    ptr1 = ptr1.next;
+	    ptr2 = ptr2.next;
+	}
+	Node newHead = ptr1.next;
+	ptr1.next = null;
+	ptr2.next = head;
+	head = newHead;
+	return head;
+    }
+
+    public Node rotateLeft(Node head, int k) {
+	if (head == null)
+	    return head;
+
+	int count = 1;
+	Node curr = head;
+
+	while (count != k) {
+	    count++;
+	    curr = curr.next;
+	}
+
+	Node newHead = curr.next;
+	curr.next = null;
+	curr = newHead;
+	while (curr.next != null) {
+	    curr = curr.next;
+	}
+
+	curr.next = head;
+	head = newHead;
+	return head;
+    }
+
+    int getLen(Node head) {
+	int count = 0;
+	while (head != null) {
+	    count++;
+	    head = head.next;
+	}
+	return count;
+    }
+
 }
